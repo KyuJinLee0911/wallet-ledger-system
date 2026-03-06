20f7a5ec2105cef136b1b4745782c3a4f5173024
rbwls1001 <rbwls1001@naver.com>
2026-03-06 16:59:41 +0900

    chore: Spring Boot 프로젝트 초기 설정 추가

---
+++ src/main/java/com/example/walletledger/config/RedisConfig.java
@@ -0,0 +1,10 @@
+package com.example.walletledger.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+
+@Configuration
+@EnableRedisRepositories
+public class RedisConfig {
+}
+
