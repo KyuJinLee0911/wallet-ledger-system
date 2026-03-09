aab537e5f45202f0ab7b799ddd723614ed64db68
rbwls1001 <rbwls1001@naver.com>
2026-03-09 16:25:10 +0900

    chore(db): transactions amount 컬럼 중복 Flyway 마이그레이션 정리

--- src/main/resources/db/migration/V3__ensure_transactions_amount_column.sql
+++
@@ -1,9 +0,0 @@
-ALTER TABLE transactions
-    ADD COLUMN IF NOT EXISTS amount NUMERIC(19,4);
-
-UPDATE transactions
-SET amount = 0
-WHERE amount IS NULL;
-
-ALTER TABLE transactions
-    ALTER COLUMN amount SET NOT NULL;
